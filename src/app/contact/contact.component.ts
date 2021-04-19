c27157d60e472e3586a6b83f633ea2684270f098
Ilia Prutkovsky <43461476+iprutkovsky@users.noreply.github.com>
2021-04-19 09:36:53 -0400

    Deploy to Heroku

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -16,10 +16,10 @@ export class ContactComponent implements OnInit {
   email: FormControl = new FormControl("", [Validators.required, Validators.email]);
   subject: FormControl = new FormControl("", [Validators.required]);
   message: FormControl = new FormControl("", [Validators.required, Validators.maxLength(256)]);
-  honeypot: FormControl = new FormControl(""); // we will use this to prevent spam
-  submitted: boolean = false; // show and hide the success message
-  isLoading: boolean = false; // disable the submit button if we're loading
-  responseMessage?: string; // the response message to show to the user
+  honeypot: FormControl = new FormControl("");
+  submitted: boolean = false;
+  isLoading: boolean = false;
+  responseMessage?: string;
 
   constructor(private appComponent: AppComponent, private formBuilder: FormBuilder, private httpClient: HttpClient) {
     appComponent.comp = false;
@@ -36,40 +36,49 @@ export class ContactComponent implements OnInit {
 
   onSubmit() {
     if (this.form.status == "VALID" && this.honeypot.value == "") {
-      this.form.disable(); // disable the form if it's valid to disable multiple submissions
+      this.form.disable();
       var formData: any = new FormData();
       formData.append("name", this.form.get("name")?.value);
       formData.append("email", this.form.get("email")?.value);
       formData.append("subject", this.form.get("subject")?.value);
       formData.append("message", this.form.get("message")?.value);
-      this.isLoading = true; // sending the post request async so it's in progress
-      this.submitted = false; // hide the response message on multiple submits
-      this.httpClient.post("https://script.google.com/macros/s/AKfycbzHo0PfzHxVCpx7g0FX6GE2WkLTOb8rNnZI-7wd2LEfPGL5H4rIC94c-8_D1eKgAqjpzA/exec", formData).subscribe(
-        (response) => {
-          // choose the response message
-          if (response == '200') {
+      this.isLoading = true;
+      this.submitted = false;
+      this.httpClient.post("https://script.google.com/macros/s/AKfycby1LkdKUhRj27O9Ofgj08SI-_32qxUYOohq7VDYSg/exec", formData).subscribe(
+        (response: any) => {
+          if (response["result"] == "success") {
             this.responseMessage = "Thanks for the message! I'll get back to you soon!";
           } else {
             this.responseMessage = "Oops! Something went wrong... Reload the page and try again.";
           }
-          this.form.enable(); // re enable the form after a success
-          this.submitted = true; // show the response message
-          this.isLoading = false; // re enable the submit button
-          console.log(response);
+          this.form.enable();
+          this.submitted = true;
+          this.isLoading = false;
         },
         (error) => {
           this.responseMessage = "Oops! An error occurred... Reload the page and try again.";
-          this.form.enable(); // re enable the form after a success
-          this.submitted = true; // show the response message
-          this.isLoading = false; // re enable the submit button
-          console.log(error);
+          this.form.enable();
+          this.submitted = true;
+          this.isLoading = false;
         }
       );
     }
   }
 
   ngAfterViewInit() {
-    $.getScript('../assets/js/validate.js');
+    // $.getScript('../assets/js/validate.js');
+    $('.fullname .email-input').on('input', function () {
+      let validateName = (name: string) => /[A-Za-z]{3,}/.test(name),
+        validateEmail = (email: string) => /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+      let name = $('.fullname'),
+        email = $('.email-input');
+
+      name.text('');
+      email.text('');
+
+      !validateName(name.val()) ? name.css('color', 'rgb(238, 53, 53)') : name.css('color', 'rgb(51, 51, 51)');
+      !validateEmail(email.val()) ? email.css('color', 'rgb(238, 53, 53)') : email.css('color', 'rgb(51, 51, 51)');
+    });
   }
 
 }
